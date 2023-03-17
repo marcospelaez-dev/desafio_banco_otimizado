347f6feeb5c61a8e2c365b1f65dd0abecb3b2698
Marcos Pelaez <64918947+marcospelaez-dev@users.noreply.github.com>
2023-03-17 11:48:46 -0300

    retificação erro código! linha 84

--- banco-otimizado.py
+++ banco-otimizado.py
@@ -81,7 +81,7 @@ def criar_conta(agencia, numero_conta, usuarios):
 
     if usuario:
         print("\n=== Conta criada com sucesso! ===")
-        return("agencia": agencia, "numero_conta": numero_conta, "usuario": usuario)
+        return{"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
     print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
 
